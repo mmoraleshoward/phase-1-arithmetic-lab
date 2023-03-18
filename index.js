11daeb2c68e1932c8e405f43efd9d38059bea4bd
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-03-18 18:58:56 -0500

    completion of arithmetic aka math is NOT fun :(

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Number.isInteger(Math.random() * 10) + 1;
+
+let num3 = 5
+let num4 = 14
+
+let mod = num4%num3
+
+let max = Math.max(5,10,15,20)
